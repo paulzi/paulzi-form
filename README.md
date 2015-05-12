da0cc7067ec5b67d741e9082dc065a7658116264
PaulZi <pavel.zimakoff@gmail.com>
2015-05-12 17:22:43 +0300

    Version 2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# paulzi-form
+# Form module
+
+## Quick start
+
+### Ajax form send
+
+Add class ```form-ajax``` to form and add ```<output>``` or ```<* class="form-output">``` tag in form.
+
+Response must return correct content-type for append to output. Else response can be processed by script.
+
+When form processing AJAX request his have class ```form-loading```. Until a response is received, the form can not be sent again.
+
+Generate events:
+- ```formajaxbefore(jqXHR, settings)``` - you can change setting and cancel request by ```preventDefault()```
+- ```formajaxdone(data, textStatus, jqXHR)``` - when ajax receive success response
+- ```formajaxfail(jqXHR, textStatus, errorThrown)``` - when ajax receive error response
+- ```formajaxalways()``` - always after receive response
+
+When an event occurs ```formajaxdone``` event with the correct content-type of response, the for document generated two events to prepare content:
+- ```contentprepare``` - before add content to DOM
+- ```contentinit``` - after add content to DOM
+
+You can ```preventDefault()``` ```formajaxdone``` event, then the response processing and adding alert to output will not occur.
+
+
+### Not send empty fields
+
+Simply add class ```form-no-empty``` to form.
+
+
+### Change status of submit button
+Add class ```.btn-loading``` to submit button. When the button is pressed, it changes the status to disabled automatically. And restores the status only after receiving the AJAX response.
+
+If you add the attribute ```data-loading-icon```, during the process form sending, the button prepended icon. Icon realized by <i> tag with specified classes in attribute.
+Example
+```html
+<!-- font awesome -->
+<button type="submit" class="btn btn-primary btn-loading" data-loading-icon="fa fa-refresh fa-spin">Submit</button>
+
+<!-- bootstrap glyph -->
+<button type="submit" class="btn btn-primary btn-loading" data-loading-icon="glyphicon glyphicon-refresh">Submit</button>
+```
+
+If you add the attribute ```data-loading-test```, during the process form sending, the button text changes to the specified in attribute.
+
+
+### Form alerts
+Use ```$.formAlert(html, type)``` for add the bootstrap alert to form output.
---
+++ paulzi-form.js
@@ -0,0 +1,175 @@
+/**
+ * PaulZi-Form module
+ * Provide: ajax submit, delete empty fields before submit, submit button style, form alert.
+ * @module paulzi/form
+ * @version 2.0.0
+ * @author PaulZi
+ */
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else {
+        factory(root.jQuery);
+    }
+}(this, function ($) {
+
+    /**
+     * Append alert to form output
+     * @memberOf jQuery.fn
+     * @param {jQuery} html - alert contents
+     * @param {string} type - alert CSS type
+     * @return {jQuery} alert element
+     */
+    $.fn.formAlert = function(html, type)
+    {
+        var output = $(this).find('output, .form-output').first();
+        
+        var div;
+        if (typeof(html)!=='string' && $(html).hasClass('alert')) {
+            div = html;
+        } else {
+            type = type || 'info';
+            div = $('<div role="alert" class="alert alert-' + type + '">')
+                .append(html);
+        }
+        
+        if ($.fn.alert) {
+            div.addClass(['alert-dismissible']);
+            $('<button type="button" class="close" aria-label="close" data-dismiss="alert">')
+                .html('<span aria-hidden="true">&times;</span>')
+                .prependTo(div);
+        }
+        
+        div.addClass('fade in').appendTo(output);
+        return div;
+    };
+    
+    
+    // btn-loading
+    var btnLoadingClickHandler = function(e) {
+        $(this).addClass('disabled');
+        
+        var loadingText = $(this).data('loadingText');
+        if (loadingText) {
+            $(this).data('loadingText', $(this).text());
+            $(this).text(loadingText);
+        }
+        
+        var loadingIcon = $(this).data('loadingIcon');
+        if (loadingIcon) {
+            $('<span>&nbsp;</span>').addClass('btn-loading-space').prependTo(this);
+            $('<i>').addClass('btn-loading-icon').addClass(loadingIcon).prependTo(this);
+        }
+    };
+    
+    var btnLoadingFormAjaxAlwaysHandler = function(e) {
+        var btn = $(this).find('.btn-loading.disabled');
+        btn.removeClass('disabled');
+        btn.children('.btn-loading-icon, .btn-loading-space').remove();
+        btn.each(function(){
+            var loadingText = $(this).data('loadingText');
+            if (loadingText) {
+                $(this).data('loadingText', $(this).text());
+                $(this).text(loadingText);
+            }
+        });
+    };
+    
+    $(document).on('click',             '.btn-loading', btnLoadingClickHandler);
+    $(document).on('formajaxalways',    '.form-ajax',   btnLoadingFormAjaxAlwaysHandler);
+    
+    
+    // form-no-empty
+    var noEmptySubmitHandler = function(e) {
+        $(this)
+            .find(':enabled')
+            .filter(function(){ return this.value==''; })
+            .addClass('form-no-empty-item')
+            .each(function(){ this.disabled = true; });
+        return true;
+    };
+    
+    var noEmptyFormAjaxAlwaysHandler = function(e) {
+        $(this)
+            .find('.form-no-empty-item')
+            .removeClass('form-no-empty-item')
+            .each(function(){ this.disabled = false; });
+    };
+    
+    $(document).on('submit',            '.form-no-empty',   noEmptySubmitHandler);
+    $(document).on('formajaxalways',    '.form-no-empty',   noEmptyFormAjaxAlwaysHandler);
+    
+    
+    // form-ajax
+    var ajaxSubmitHandler = function(e) {
+        if (e.isDefaultPrevented()) return false;
+        e.preventDefault();
+        
+        var form = $(this);
+        if (form.hasClass('form-loading')) return false;
+        
+        form.addClass('form-loading');
+        var output = form.find('output').empty();
+        
+        $.ajax({
+                method:     form.attr('method') || 'GET',
+                url:        form.attr('action'),
+                data:       form.serialize(),
+                beforeSend: function(jqXHR, settings) {
+                    var event = jQuery.Event("formajaxbefore");
+                    form.trigger(event, [jqXHR, settings]);
+                    return !event.isDefaultPrevented();
+                }
+            })
+            .done(function(data, textStatus, jqXHR)
+            {
+                var event = jQuery.Event("formajaxdone");
+                form.trigger(event, arguments);
+                if (event.isDefaultPrevented()) return;
+                
+                if (typeof(data)!=='string') return;
+                data = $(data);
+                var redirect = data.data('redirect');
+                if (redirect) {
+                    document.location.href = redirect;
+                    return;
+                }
+                $(document).trigger('contentprepare', [data]);
+                if (data.hasClass('form-replace')) {
+                    form.replaceWith(data);
+                } else {
+                    if (data.hasClass('alert')) {
+                        form.formAlert(data);
+                    } else {
+                        output.html(data);
+                    }
+                }
+                $(document).trigger('contentinit', [data]);
+            })
+            .fail(function(jqXHR, textStatus, errorThrown)
+            {
+				var event = jQuery.Event("formajaxfail");
+                form.trigger(event, arguments);
+                if (event.isDefaultPrevented()) return;
+                
+                var alert = false;
+                if (jqXHR.responseText) alert = $(jqXHR.responseText);
+                if (alert && alert.hasClass('alert')) {
+                    form.formAlert(alert);
+                } else {
+                    form.formAlert(['[' + textStatus + ']', errorThrown, jqXHR.responseText].join(' '), 'danger');
+                }
+            })
+            .always(function(){
+                var event = jQuery.Event("formajaxalways");
+                form.trigger(event, arguments);
+                if (event.isDefaultPrevented()) return;
+                
+                form.removeClass('form-loading');
+            });
+            
+        return false;
+    };
+    
+    $(document).on('submit', '.form-ajax', ajaxSubmitHandler);
+}));
