9481df77e306481a947a65d76308a06d6f1deb8f
PaulZi <pavel.zimakoff@gmail.com>
2015-06-25 02:49:57 +0300

    Version 2.4.0. Code style and refactoring.

--- README.md
+++ README.md
@@ -1,66 +1,62 @@
-# Form module
+# PaulZi Form module
 
-## Quick start
+## Html5 form* attributes polyfill
+Use `form`, `formaction`, `formenctype`, `formmethod`, `formtarget` attributes! This script will provide support for older browsers.
 
+## Changing the behavior of all forms
+When form is submitting, it has class `form-loading`. Until a response is received, the form can not be submit again.
 
-### html5 form* attributes polyfill
-Use `form`, `formaction`, `formenctype`, `formmethod`, `formtarget` attributes! This script will support older browsers.
-
-
-### All forms
-
-When form is processing, it has class ```form-loading```. Until a response is received, the form can not be sent again.
-
-### Ajax form send
-
-This method support active submit button names and `<input type="image">` `x`, `y` parameters. Also support html5 form* attributes for override form action and method.
-Add class ```form-ajax``` to form and add ```<output>``` or ```<* class="form-output">``` tag in form.
-You can use `data-form-output` for set global output content.
-Response must return correct content-type text/html for append to output. Else response can be processed by script.
+## Ajax form send
+Provides submit the form via ajax. It supports the transfer of the name active submitting button and `<input type="image">` `x`, `y` parameters. Also support html5 form* attributes for override form action and method.
+For use, add class `form-ajax` to form and add `<output>` or `<* class="form-output">` tag in form. You can use `data-form-output` attribute for find output element globally in document.
+Response must return correct content-type `text/html` for append to output, otherwise it can be handled only by JavaScript.
 
 Generate events:
-- ```formajaxbefore(jqXHR, settings)``` - you can change setting and cancel request by ```preventDefault()```
-- ```formajaxdone(data, textStatus, jqXHR)``` - when ajax receive success response
-- ```formajaxfail(jqXHR, textStatus, errorThrown)``` - when ajax receive error response
-- ```formajaxalways()``` - always after receive response
+- `formajaxbefore (jqXHR, settings)` - you can change settings and cancel request by `preventDefault()`;
+- `formajaxdone (data, textStatus, jqXHR)` - when ajax receive success response;
+- `formajaxfail (jqXHR, textStatus, errorThrown)` - when ajax receive error response;
+- `formajaxalways()` - always after receive response.
+All defined in the script event behavior can be canceled by `preventDefault()`.
 
-When an event occurs ```formajaxdone``` event with the correct content-type of response, the for document generated two events to prepare content:
-- ```contentprepare``` - before add content to DOM
-- ```contentinit``` - after add content to DOM
-
-You can ```preventDefault()``` ```formajaxdone``` event, then the response processing and adding alert to output will not occur.
-If you specify `form-replace` class to root tag, all form will be replace by output.
-If root tag is has class `alert`, it we automate make `alert-dismissible` if bootstrap is included and generate `formAlert()` (see below).
-
-### Redirect after AJAX
-Set X-Redirect header in response for redirect page. You can stop redirect, if `preventDefault()` `formajaxdone` or `formajaxfail` event.
+At event `formajaxdone`, when the content is added to the output, for form generated two events ti prepare content:
+- `contentprepare` - before add content to DOM
+- `contentinit` - after add content to DOM
+Through this, you can initialize all JavaScript components such as page load, and when Ajax loading. Simple use:
+```javascript
+$(function () {
+    $(document).trigger('preparecontent', [$(document)]);
+}
+$(document).on('preparecontent', function (cont) {
+    cont.find('.js-component').jsComponent();
+});
+```
+If you specify a class `form-replace` to root element of response, the form will replace by contents. You can also specify `data-form-output` attribute in root element of response to override the output element.
+If the root element has class `alert`, it we automate make `alert-dismissible` if bootstrap javascript is included on page.
 
 ### Ajax form with files
 With jQuery Form plugin (https://github.com/malsup/form/) you can send files with fallback for browsers, not supported XMLHttpRequest Level 2.
-To use, simply specify enctype="multipart/form-data" and connect jQuery Form plug-in, the script will detect that you want to use this plug-in.
-Additional send parameter `X-Requested-With` with value `XMLHttpRequest`, for detect iframe method of form submit.
+For use, simply specify `enctype="multipart/form-data"` and include jQuery Form plug-in on page, the script will automatically detect that you want to use this plug-in.
+Additionally, sent parameter `X-Requested-With` with value `XMLHttpRequest`, for detect iframe method of form submit.
 
 Generates additional events:
-- ```formajaxprogress(loaded, total, percent)``` - when ajax uploading progress
+- `formajaxprogress (loaded, total, percent)` - when ajax uploading progress
 
-### Support submit buttons with name or type=image
-Support submit buttons with name and ```input[type="image"]``` mouse click offset.
+### Redirect after AJAX
+Set X-Redirect header in response for redirect page. You can stop redirect, if `preventDefault()` in `formajaxdone` or `formajaxfail` events.
 
-### Not send empty fields
-
-Simply add class ```form-no-empty``` to form.
+### Do not send empty fields
+Just add a class `form-no-empty` to form.
 
 ### No ajax button
-
-If you want to send a form normally, only some button, simply add `btn-no-ajax` class to this buttons.
+If you want to submit a form normally, but only some buttons, add `btn-no-ajax` class to this buttons.
  
+ 
+## Change status of submit button
+Add class `.btn-loading` to submit button. When the button is pressed, it changes the status to disabled automatically, and restores the status only after receiving the AJAX response.
+Add class `.btn-submit-default` to submit button, then it will be used as the target of a form submit from the keyboard.
 
-### Change status of submit button
-Add class ```.btn-loading``` to submit button. When the button is pressed, it changes the status to disabled automatically. And restores the status only after receiving the AJAX response.
-Add class ```.btn-submit-default``` to submit button, then it will be used as the target of a form submit from the keyboard.
-
-If you add the attribute ```data-loading-icon```, during the process form sending, the button prepended icon. Icon realized by <i> tag with specified classes in attribute.
-Example
+If you add the attribute `data-loading-icon`, during the process form sending, the button prepended icon. Icon realized by <i> tag with specified classes in attribute.
+Example:
 ```html
 <!-- font awesome -->
 <button type="submit" class="btn btn-primary btn-loading" data-loading-icon="fa fa-refresh fa-spin">Submit</button>
@@ -69,8 +65,7 @@ Example
 <button type="submit" class="btn btn-primary btn-loading" data-loading-icon="glyphicon glyphicon-refresh">Submit</button>
 ```
 
-If you add the attribute ```data-loading-text```, during the process form sending, the button text changes to the specified in attribute.
+If you add the attribute `data-loading-text`, during the process form sending, the button text changes to the specified in attribute.
 
-
-### Form alerts
-Use ```$.formAlert(html, type)``` for add the bootstrap alert to form output.
+## Form alerts
+Use `$.formAlert(elem, type)` for add the bootstrap alert to form output.
--- paulzi-form.js
+++ paulzi-form.js
@@ -1,11 +1,12 @@
 /**
  * PaulZi-Form module
  * Provide: ajax submit, delete empty fields before submit, submit button style, form alert.
- * @module paulzi/form
- * @version 2.3.1
+ * @module paulzi/paulzi-form
+ * @external jQuery
+ * @version 2.4.0
  * @author PaulZi (pavel.zimakoff@gmail.com)
- * @license MIT (https://github.com/Paul-Zi/paulzi-form/blob/master/LICENSE)
- * @see https://github.com/Paul-Zi/paulzi-form
+ * @license MIT (https://github.com/paulzi/paulzi-form/blob/master/LICENSE)
+ * @see https://github.com/paulzi/paulzi-form
  */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -15,123 +16,125 @@
     }
 }(this, function ($) {
 
+    // internal functions
+    var getFormOutput = function ($form) {
+        var $output = $form.find('output, .form-output').first();
+        if ($form.hasData('formOutput')) {
+            $output = $($form.data('formOutput'));
+        }
+        return $output;
+    };
+
+    var isSubmit = function ($item) {
+        return $.inArray($item.prop('tagName'), ['INPUT', 'BUTTON']) !== -1
+            && $.inArray($item.attr('type').toLowerCase(), ['submit', 'image']) !== -1;
+    };
+
+
     /**
      * Append alert to form output
      * @memberOf jQuery.fn
-     * @param {jQuery} html - alert contents
+     * @param {jQuery} elem - alert contents
      * @param {string} type - alert CSS type
-     * @return {jQuery} alert element
+     * @returns {jQuery} alert element
      */
-    $.fn.formAlert = function(html, type)
-    {
-        var output = $(this).find('output, .form-output').first();
-        
-        var div;
-        if (typeof(html) !== 'string' && $(html).hasClass('alert')) {
-            div = html;
-        } else {
+    $.fn.formAlert = function (elem, type) {
+
+        var $form = $(this);
+        var $elem = $(elem);
+        if (!$elem.hasClass('alert')) {
             type = type || 'info';
-            div = $('<div role="alert" class="alert alert-' + type + '">')
-                .append(html);
+            $elem = $('<div role="alert" class="alert alert-' + type + '">').append($elem);
         }
         
-        if ($.fn.alert) {
-            div.addClass(['alert-dismissible']);
+        if ($.fn.alert && !$elem.hasClass('alert-dismissible')) {
+            $elem.addClass('alert-dismissible');
             $('<button type="button" class="close" aria-label="close" data-dismiss="alert">')
                 .html('<span aria-hidden="true">&times;</span>')
-                .prependTo(div);
+                .prependTo($elem);
         }
         
-        div.addClass('fade in').appendTo(output);
-        return div;
+        $elem.addClass('fade in').appendTo(getFormOutput($form));
+        return $elem;
     };
-    
-    
-    // is submit
-    var isSubmit = function(item) {
-        return $.inArray(item.prop('tagName'), ['INPUT', 'BUTTON']) !== -1
-            && $.inArray(item.attr('type').toLowerCase(), ['submit', 'image']) !== -1;
-    };
-    
-    
+
+
     // btn-loading
-    var btnLoadingSubmitHandler = function(e) {
+    var btnLoadingSubmitHandler = function (e) {
         if (e.isDefaultPrevented()) {
             return false;
         }
-        if ($(this).hasClass('form-loading')) {
+        var $this = $(this);
+        if ($this.hasClass('form-loading')) {
             e.preventDefault();
             return false;
         }
-        $(this).addClass('form-loading');
+        $this.addClass('form-loading');
         
-        var btn = $(document.activeElement).filter('.btn-loading');
-        if (!isSubmit(btn)) {
-            btn = $(this).find('.btn-loading.btn-submit-default');
+        var $btn = $(document.activeElement).filter('.btn-loading');
+        if (!isSubmit($btn)) {
+            $btn = $this.find('.btn-loading.btn-submit-default');
         }
-        btn.addClass('disabled');
+        $btn.addClass('disabled');
         
-        var loadingText = btn.data('loadingText');
+        var loadingText = $btn.data('loadingText');
         if (loadingText) {
-            btn.data('loadingText', btn.text());
-            btn.text(loadingText);
+            $btn.data('loadingText', $btn.text());
+            $btn.text(loadingText);
         }
         
-        var loadingIcon = btn.data('loadingIcon');
+        var loadingIcon = $btn.data('loadingIcon');
         if (loadingIcon) {
-            $('<span>&nbsp;</span>').addClass('btn-loading-space').prependTo(btn);
-            $('<i>').addClass('btn-loading-icon').addClass(loadingIcon).prependTo(btn);
+            $('<span>&nbsp;</span>').addClass('btn-loading-space').prependTo($btn);
+            $('<i>').addClass('btn-loading-icon').addClass(loadingIcon).prependTo($btn);
         }
     };
     
-    var btnLoadingFormAjaxAlwaysHandler = function(e) {
-        var btn = $(this).find('.btn-loading.disabled');
-        btn.removeClass('disabled');
-        btn.children('.btn-loading-icon, .btn-loading-space').remove();
-        btn.each(function(){
-            var loadingText = $(this).data('loadingText');
+    var btnLoadingFormAjaxAlwaysHandler = function (e) {
+        var $btn = $(this).find('.btn-loading.disabled');
+        $btn.removeClass('disabled');
+        $btn.children('.btn-loading-icon, .btn-loading-space').remove();
+        $btn.each(function () {
+            var $this = $(this);
+            var loadingText = $this.data('loadingText');
             if (loadingText) {
-                $(this).data('loadingText', $(this).text());
-                $(this).text(loadingText);
+                $this.data('loadingText', $(this).text()).text(loadingText);
             }
         });
     };
     
     $(document).on('submit.paulzi-form',         'form',         btnLoadingSubmitHandler);
     $(document).on('formajaxalways.paulzi-form', '.form-ajax',   btnLoadingFormAjaxAlwaysHandler);
-    
 
+
+    // form html5 attributes polyfill
     var _formAttrSupport;
 
     var formAttrList = ['action', 'enctype', 'method', 'target'];
 
-    var formAttrSupport = function() {
+    var formAttrSupport = function () {
         if (typeof(_formAttrSupport) !== 'undefined') {
             return _formAttrSupport;
         }
         var input   = document.createElement('input');
         var form    = document.createElement('form');
-        var formId  = '__test-input-form-reference-support';
+        var formId  = 'paulzi-form-support';
         form.id = formId;
         document.body.appendChild(form);
         document.body.appendChild(input);
         input.setAttribute('form', formId);
-        //for (var i=0; i < form.length; i++) {
-        //    alert('support: ' + (form[i] == input));
-        //}
         _formAttrSupport = (input.form === form);
         document.body.removeChild(form);
         document.body.removeChild(input);
         return _formAttrSupport;
     };
 
-    var formActionAttrSupport = function() {
+    var formActionAttrSupport = function () {
         var input = document.createElement("input");
         return !!('formAction' in input);
     };
 
-    // form html5 attributes polyfill
-    var attrPolyfillSubmitHandler = function(e) {
+    var attrPolyfillSubmitHandler = function (e) {
         var that  = this;
         var $form = $(this);
         if (!formAttrSupport()) {
@@ -139,9 +142,9 @@
             if (id) {
                 var list = $('[form="' + id + '"]')
                     .not('form#' + id + ' [form="' + id + '"]')
-                    .filter(function(){
+                    .filter(function () {
                         return this.name && !$(this).is(':disabled') && (this.checked || (this.type !== 'checkbox' && this.type !== 'radio'));
-                    }).each(function(){
+                    }).each(function () {
                         $('<input type="hidden">')
                             .attr('name', this.name)
                             .val($(this).val())
@@ -151,20 +154,21 @@
             }
         }
         if (!formActionAttrSupport()) {
-            var btn = $(document.activeElement);
-            if (isSubmit(btn)) {
+            var $btn = $(document.activeElement);
+            if (isSubmit($btn)) {
                 $.each(formAttrList, function (i, a) {
-                    var attr = btn.attr('form' + a);
+                    var attr = $btn.attr('form' + a);
                     if (attr) {
-                        $form.data('paulziFormPolyfill' + a, attr);
-                        $form.attr(a, attr);
+                        $form
+                            .data('paulziFormPolyfill' + a, attr)
+                            .attr(a, attr);
                     }
                 });
             }
         }
     };
 
-    var attrPolyfillFormAjaxAlwaysHandler = function(e) {
+    var attrPolyfillFormAjaxAlwaysHandler = function (e) {
         var $form = $(this);
         if (!formAttrSupport()) {
             $form.find('.paulzi-form-polyfill').remove();
@@ -172,8 +176,9 @@
         if (!formActionAttrSupport()) {
             $.each(formAttrList, function (i, a) {
                 if ($form.hasData('paulziFormPolyfill' + a)) {
-                    $form.attr(a, $form.data('paulziFormPolyfill' + a));
-                    $form.removeData('paulziFormPolyfill' + a);
+                    $form
+                        .attr(a, $form.data('paulziFormPolyfill' + a))
+                        .removeData('paulziFormPolyfill' + a);
                 }
             });
         }
@@ -184,20 +189,22 @@
 
 
     // form-no-empty
-    var noEmptySubmitHandler = function(e) {
+    var noEmptyItemClass = 'paulzi-form-no-empty-item';
+    
+    var noEmptySubmitHandler = function (e) {
         $(this)
             .find(':enabled')
-            .filter(function(){ return this.value==''; })
-            .addClass('form-no-empty-item')
-            .each(function(){ this.disabled = true; });
+            .filter(function () { return this.value==''; })
+            .addClass(noEmptyItemClass)
+            .each(function () { this.disabled = true; });
         return true;
     };
     
-    var noEmptyFormAjaxAlwaysHandler = function(e) {
+    var noEmptyFormAjaxAlwaysHandler = function (e) {
         $(this)
-            .find('.form-no-empty-item')
-            .removeClass('form-no-empty-item')
-            .each(function(){ this.disabled = false; });
+            .find('.' + noEmptyItemClass)
+            .removeClass(noEmptyItemClass)
+            .each(function () { this.disabled = false; });
     };
     
     $(document).on('submit.paulzi-form',         '.form-no-empty',   noEmptySubmitHandler);
@@ -205,13 +212,14 @@
     
     
     // form-ajax
-    var inputImageClickHandler = function(e) {
+    var inputImageClickHandler = function (e) {
         var offset = $(this).offset();
-        $(this).prop('paulziFormX', e.pageX - offset.left);
-        $(this).prop('paulziFormY', e.pageY - offset.top);
+        $(this)
+            .prop('paulziFormX', e.pageX - offset.left)
+            .prop('paulziFormY', e.pageY - offset.top);
     };
     
-    var ajaxSubmitHandler = function(e) {
+    var ajaxSubmitHandler = function (e) {
         if (e.isDefaultPrevented()) {
             return false;
         }
@@ -219,23 +227,26 @@
             return true;
         }
         e.preventDefault();
+
+        var $form = $(this);
+
+        var redirect = function () {
+            var redirect = jqXHR.getResponseHeader('X-Redirect');
+            if (redirect) {
+                document.location.href = redirect;
+                return true;
+            }
+            return false;
+        };
         
-        var form = $(this);
-        var output = form.find('output, .form-output').empty();
-        if (form.data('form-output')) {
-            output = $(form.data('form-output'));
-        }
-        
-        var doneCallback = function(data, textStatus, jqXHR) {
+        var doneCallback = function (data, textStatus, jqXHR) {
             var event = jQuery.Event("formajaxdone");
-            form.trigger(event, arguments);
+            $form.trigger(event, arguments);
             if (event.isDefaultPrevented()) {
                 return;
             }
 
-            var redirect = jqXHR.getResponseHeader('X-Redirect');
-            if (redirect) {
-                document.location.href = redirect;
+            if (redirect()) {
                 return;
             }
 
@@ -243,100 +254,102 @@
                 return;
             }
 
-            data = $($.parseHTML(data, true));
-            form.trigger('contentprepare', [data]);
-            if (data.hasClass('form-replace')) {
-                form.replaceWith(data);
+            var $data = $($.parseHTML(data, true));
+            $form.trigger('contentprepare', [$data]);
+            if ($data.hasClass('form-replace')) {
+                $form.replaceWith($data);
             } else {
-                if (data.hasClass('alert')) {
-                    form.formAlert(data);
-                } else {
-                    output.html(data);
+                var $output = getFormOutput($form);
+                var dataOutput = $data.data('formOutput');
+                if (dataOutput) {
+                    $output = $(dataOutput);
                 }
+                if ($data.hasClass('alert')) {
+                    $data = $form.formAlert($data);
+                }
+                $output.html($data);
             }
-            form.trigger('contentinit', [data]);
+            $form.trigger('contentinit', [$data]);
         };
         
-        var failCallback = function(jqXHR, textStatus, errorThrown) {
+        var failCallback = function (jqXHR, textStatus, errorThrown) {
             var event = jQuery.Event("formajaxfail");
-            form.trigger(event, arguments);
+            $form.trigger(event, arguments);
             if (event.isDefaultPrevented()) {
                 return;
             }
 
-            var redirect = jqXHR.getResponseHeader('X-Redirect');
-            if (redirect) {
-                document.location.href = redirect;
+            if (redirect()) {
                 return;
             }
             
-            var alert = false;
+            var $alert = false;
             if (jqXHR.responseText) {
-                alert = $($.parseHTML(jqXHR.responseText, true));
+                $alert = $($.parseHTML(jqXHR.responseText, true));
             }
-            if (alert && alert.hasClass('alert')) {
-                form.formAlert(alert);
+            if ($alert && $alert.hasClass('alert')) {
+                $form.formAlert($alert);
             } else {
-                form.formAlert(['[' + textStatus + ']', errorThrown, jqXHR.responseText].join(' '), 'danger');
+                $form.formAlert(['[' + textStatus + ']', errorThrown, jqXHR.responseText].join(' '), 'danger');
             }
         };
         
-        var alwaysCallback = function() {
+        var alwaysCallback = function () {
             var event = jQuery.Event("formajaxalways");
-            form.trigger(event, arguments);
+            $form.trigger(event, arguments);
             if (event.isDefaultPrevented()) {
                 return;
             }
-            
-            form.removeClass('form-loading');
+
+            $form.removeClass('form-loading');
         };
         
-        var beforeSendCallback = function(jqXHR, settings) {
+        var beforeSendCallback = function (jqXHR, settings) {
             var event = jQuery.Event("formajaxbefore");
-            form.trigger(event, [jqXHR, settings]);
+            $form.trigger(event, [jqXHR, settings]);
             return !event.isDefaultPrevented();
         };
         
-        if (form.attr('enctype') == 'multipart/form-data' && typeof($.fn.ajaxSubmit) === 'function') {
+        if ($form.attr('enctype') == 'multipart/form-data' && typeof($.fn.ajaxSubmit) === 'function') {
             // if exists file in form and included jquery.form plugin, use it. @see: http://malsup.com/jquery/form/
-            form.ajaxSubmit({
-                beforeSubmit: function(arr, $form, options) {
+            $form.ajaxSubmit({
+                beforeSubmit: function (arr, form, options) {
                     options.beforeSend = beforeSendCallback;
                 },
                 success:        doneCallback,
                 error:          failCallback,
                 complete:       alwaysCallback,
                 data:           {"X-Requested-With": "XMLHttpRequest"},
-                uploadProgress: function(event) {
+                uploadProgress: function (event) {
                     var percent = 0;
                     var loaded = event.loaded || event.position;
                     var total = event.total;
                     if (event.lengthComputable) {
                         percent = loaded / total * 100;
                     }
-                    form.trigger('formajaxprogress', [loaded, total, percent]);
+                    $form.trigger('formajaxprogress', [loaded, total, percent]);
                 }
             });
         } else {
-            var url     = form.attr('action');
-            var method  = form.attr('method') || 'GET';
-            var data    = form.serializeArray();
-            var btn     = $(document.activeElement);
-            if (btn.attr('name') && !btn.is(":disabled") && isSubmit(btn)) {
-                if (btn.attr('formaction')) {
-                    url = btn.attr('formaction');
+            var url     = $form.attr('action');
+            var method  = $form.attr('method') || 'GET';
+            var data    = $form.serializeArray();
+            var $btn     = $(document.activeElement);
+            if ($btn.attr('name') && !$btn.is(":disabled") && isSubmit($btn)) {
+                if ($btn.attr('formaction')) {
+                    url = $btn.attr('formaction');
                 }
-                if (btn.attr('formmethod')) {
-                    method = btn.attr('formmethod');
+                if ($btn.attr('formmethod')) {
+                    method = $btn.attr('formmethod');
                 }
-                data.push({name: btn.attr('name'), value: btn.val()});
-                if (btn.prop('paulziFormX')) {
-                    data.push({name: btn.attr('name') + '.x', value: btn.prop('paulziFormX')});
-                    btn.removeProp('paulziFormX');
+                data.push({name: $btn.attr('name'), value: $btn.val()});
+                if ($btn.prop('paulziformX')) {
+                    data.push({name: $btn.attr('name') + '.x', value: $btn.prop('paulziformX')});
+                    $btn.removeProp('paulziformX');
                 }
-                if (btn.prop('paulziFormY')) {
-                    data.push({name: btn.attr('name') + '.y', value: btn.prop('paulziFormY')});
-                    btn.removeProp('paulziFormY');
+                if ($btn.prop('paulziformY')) {
+                    data.push({name: $btn.attr('name') + '.y', value: $btn.prop('paulziformY')});
+                    $btn.removeProp('paulziformY');
                 }
             }
             $.ajax({
